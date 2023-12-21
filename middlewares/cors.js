7bdb56ffa6a772e32bc70bfb6c830d575e11c8d4
Wilmer Maldonado <wilmer.payall@gmail.com>
2023-12-21 19:48:12 -0400

    add http://127.0.0.1:5173 origin

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,8 @@
 import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
-  'http://localhost:5173'
+  'http://localhost:5173',
+  'http://127.0.0.1:5173'
 ]
 
 export const corsMiddlewares = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
